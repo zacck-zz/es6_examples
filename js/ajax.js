6c00ef98fa864528988bc8b8947c1dce0dcf6cbf
Zacck <zacck@Zaccks-MacBook-Pro.local>
2016-06-17 17:31:49 +0200

    added functionality for uploading data and worked on some refactoring

--- js/ajax.js
+++ js/ajax.js
@@ -2,14 +2,52 @@ $(function() {
   //cache the dom
   var $orders = $('#orders');
 
-  //call the ajax
+  var $name = $('#name');
+  var $drink = $('#drink');
+
+  //refactor
+  function addOrder(curritem)
+  {
+    $orders.append('<li>name: '+ curritem.name+', drink: '+curritem.drink +'</li>');
+
+  }
+
+
+  //call the ajax to get all existing orders
   $.ajax({
     type: 'GET',
     url: 'http://rest.learncode.academy/api/learncode/friends',
     success: function(response){
       $.each(response, function(i, curritem){
-        $orders.append('<li>name: '+ curritem.name+', drink: '+curritem.drink +'<li>');
+        addOrder(curritem);
       });
+    },
+    error: function() {
+      alert('error loading orders');
     }
   });
+
+
+  //use this to post to the database
+  $('#add-order').on('click', function(){
+    //collect data to post
+    var postItem = {
+      name: $name.val(),
+      drink: $drink.val()
+    }
+    $.ajax({
+      type: 'POST',
+      url: 'http://rest.learncode.academy/api/learncode/friends',
+      data: postItem,
+      success: function(newOrder){
+        addOrder(newOrder);
+      },
+      error: function(){
+        alert('error adding current order');
+      }
+    });
+
+
+  });
+
 });
