58bc1f23bcf013e4b17c69b0654f53443fc2a05b
Zacck <zacck@Zaccks-MacBook-Pro.local>
2016-06-17 13:06:52 +0200

    worked on event listeners

--- js/app.js
+++ js/app.js
@@ -12,5 +12,45 @@ $(function() {
     fontWeight: 'bold'
   });
 
+  //Event Binding
+  $('#btnevent1').html('my button 1');
+
+  $('#panelevent1').html('mypanel <strong> my bold</strong>');
+
+  //event from one element to another
+  $('#btnevent1').on('mouseover', function(){
+    $('#panelevent1').fadeOut();
+  });
+
+  $('#btnevent3').on('mouseover', function(){
+    $('#panelevent3').fadeOut();
+  });
+  $('#btnevent4').on('mouseover', function(){
+    $('#panelevent4').fadeOut();
+  });
+
+  //event from one element to another
+  $('#btnevent2').on('click', function(){
+    $('#panelevent2 .panel-content').html('my new panel content');
+  });
+  $('#btnevent1').on('click', function(){
+    $('#panelevent1').fadeIn();
+  });
+  $('#btnevent3').on('click', function(){
+    $('#panelevent3').fadeIn();
+  });
+  $('#btnevent4').on('click', function(){
+    $('#panelevent4').fadeIn();
+  });
+
+
+
+
+  //event on same element
+  $('#panelevent1').on('click', function(){
+    $('#panelevent1').slideToggle(200);
+  });
+
+
 
 });
