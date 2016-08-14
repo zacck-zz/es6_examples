cc74bb51663cb302501039a7ec05d3938b9e0c60
Zacck <zacck@Zaccks-MacBook-Pro.local>
2016-08-14 20:35:45 +0200

    worked on functionality for child changed, added and removed using firebase  realtime database on the web

--- js/app.js
+++ js/app.js
@@ -10,9 +10,11 @@
 
   //get elements
   const preObject = document.getElementById('object');
+  const listObject = document.getElementById('hobbies');
 
   //Create Db reference
   const dbRefObject = firebase.database().ref().child('object');
+  const dbRefList = dbRefObject.child('hobbies');
   /*
   * Ref gets you to the root of the database
   * child then creates or finds the node specified
@@ -24,5 +26,25 @@
   dbRefObject.on('value', snap => {
     preObject.innerText = JSON.stringify(snap.val(), null, 3);
     });
+
+  //Using child events to sync when a a child is added
+  dbRefList.on('child_added', snap => {
+    const li = document.createElement('li');
+    li.innerText = snap.val();
+    li.id = snap.key;
+    listObject.appendChild(li);
+  });
+
+  //check when children are updated
+  dbRefList.on('child_changed', snap => {
+    const liChanged = document.getElementById(snap.key);
+    liChanged.innerText = snap.val();
+  });
+  //check when children are deleted
+  dbRefList.on('child_removed', snap => {
+    const liToremove = document.getElementById(snap.key);
+    liToremove.remove();
+  });
+
 
 }());
